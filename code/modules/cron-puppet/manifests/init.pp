550af0e84b01ca7f865583ad510807ded8056697
Steve VanDevender <stevev@hexadecimal.uoregon.edu>
2019-07-10 15:16:53 -0700

    add puppet cron job

---
+++ code/modules/cron-puppet/manifests/init.pp
@@ -0,0 +1,8 @@
+class cron-puppet {
+	# update /etc/puppet and run "puppet apply":
+   	cron { "puppet apply":
+		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
+		user    => root,
+		minute  => "*/5",
+	}
+}
