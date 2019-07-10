e2683c1a01313c518f0a9a70a5dc3c81e6e7c0a6
Steve VanDevender <stevev@hexadecimal.uoregon.edu>
2019-07-10 15:29:54 -0700

    fix class name

--- code/modules/cron_puppet/manifests/init.pp
+++ code/modules/cron_puppet/manifests/init.pp
@@ -1,4 +1,4 @@
-class cron-puppet {
+class cron_puppet {
 	# update /etc/puppet and run "puppet apply":
    	cron { "puppet apply":
 		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
